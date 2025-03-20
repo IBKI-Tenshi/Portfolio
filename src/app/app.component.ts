a964a6d71b8446c955b02dc636f02437e6318773
IBKI-Tenshi <kitak.borna@gmail.com>
2025-03-21 00:10:21 +0100

    zwischenspeichern

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { StartScreenComponent } from './start-screen/start-screen.component';
-import { AboutMeComponent } from "./about-me/about-me.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, StartScreenComponent, AboutMeComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,24 +1,13 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../shared/header/header.component';
+import { HeroComponent } from "../hero/hero.component";
+import { AboutMeComponent } from "../about-me/about-me.component";
 
 @Component({
   selector: 'app-start-screen',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeroComponent, AboutMeComponent],
   templateUrl: './start-screen.component.html',
   styleUrl: './start-screen.component.scss'
 })
 export class StartScreenComponent {
-  // buttonText: string = "Hello World"; // Standard-Text
-  // // buttonWidth: string = "140px"; // Standard-Breite
-
-  // changeText() {
-  //   this.buttonText = "I'M Borna Kitak"; // Neuer Text
-  //   // this.buttonWidth = "220px"; // Breite für längeren Text
-  // }
-
-  // resetText() {
-  //   this.buttonText = "Hello World"; // Zurücksetzen des Textes
-  //   // this.buttonWidth = "140px"; // Zurücksetzen der Breite
-  // }
 }
