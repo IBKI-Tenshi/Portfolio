b9f0ae7f49713858d7921126e17f50c1d5a5cbad
IBKI-Tenshi <kitak.borna@gmail.com>
2025-04-07 21:50:37 +0200

    überschrift mit opacität animieren als nächstes

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
   imports: [],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+  styleUrls: ['./about-me.component.scss']
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements AfterViewInit {
 
+  @ViewChild('underline_img') underlineImg!: ElementRef<HTMLImageElement>;
+
+  ngAfterViewInit(): void {
+    const el = this.underlineImg.nativeElement;
+
+    const toggleEffect = () => {
+      el.classList.add('show_underline');
+      setTimeout(() => {
+        el.classList.remove('show_underline');
+      }, 1000);
+    };
+
+    toggleEffect();
+    setInterval(toggleEffect, 4000);
+  }
 }
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
+import { NgClass } from '@angular/common';
+
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, NgClass],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
 
+  hovered: boolean = false;
+
+  onHover(isHovered: boolean): void {
+    this.hovered = isHovered;
+  }
   
 }
